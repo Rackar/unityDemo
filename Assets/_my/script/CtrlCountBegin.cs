6041e6fbbdde66b934446dce76766a10bcaee467
Your Name <27681135@qq.com>
2019-07-16 16:44:58 +0800

    增加

---
+++ Assets/_my/script/CtrlCountBegin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlCountBegin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemy;
+    void Start()
+    {
+        int t = Random.Range(1, 10);
+        print(t);
+        for (int i = 0; i < t; i++)
+        {
+            GameObject eachEnemy = Instantiate(enemy, new Vector3(i - t + t / 2, 6, 0), Quaternion.identity) as GameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/_my/script/enemyGoDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyGoDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 4;
+    void Start()
+    {
+        // transform.position = new Vector3(0, 6, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 1)
+        {
+            transform.Translate(Vector3.up * -speed * Time.deltaTime);
+        }
+        else
+        {
+            // transform.position = new Vector3(0, 10, 0);
+        }
+    }
+}
