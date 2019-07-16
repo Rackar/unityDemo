67b4149e7f49a2d47a15fd6227322b1790421d8d
Your Name <27681135@qq.com>
2019-07-16 15:51:38 +0800

    init

---
+++ Assets/_my/script/enemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 4;
+    void Start()
+    {
+        transform.position = new Vector3(22, 3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > -22)
+        {
+            transform.Translate(Vector3.right * -speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = new Vector3(22, 3, 0);
+        }
+    }
+}
